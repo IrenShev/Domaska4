c6f80b21041afed386cff89bdd32a3f901444044
Iren <i.shev3456@gmail.com>
2018-03-10 12:07:11 +0200

    test

--- src/com/company/dz9/Matrix.java
+++ src/com/company/dz9/Matrix.java
@@ -3,5 +3,6 @@ package com.company.dz9;
 public class Matrix {
     public static void main(String[] args) {
         System.out.println("hello git!)");
+        System.out.println("hello git!)");
     }
 }
